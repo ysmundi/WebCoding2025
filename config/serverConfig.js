2e70b8211ce5a4ceb8d52eb4da4037061235dd39
charliecaiftaa <charlie.cai@g.feitianacademy.org>
2025-01-22 12:39:42 -0500

    adding files

---
+++ config/serverConfig.js
@@ -0,0 +1,129 @@
+// config/database.js
+let serverConfig = {
+    commondb_connection: {
+        'multipleStatements': true,
+        'connectionLimit' : 100,
+        // 'host': 'localhost',
+        'host': '10.11.90.15',
+        'user': 'AppUser',
+        'password': 'Special888%',
+        'port'    :  3306
+    },
+    session_connection: {
+        'multipleStatements': true,
+        'connectionLimit' : 100,
+        // 'host': 'localhost',
+        'host': '10.11.90.15',
+        'user': 'AppUser',
+        'password': 'Special888%',
+        'port'    :  3306
+    },
+
+    Session_db: 'ESP2',
+    Login_db: 'ESP2',
+    Login_table: 'UserLogin',
+    Upload_db: 'ESP2',
+
+    Server_Port: 9092,
+
+    // local_URL : "",
+    // local_URL : "http://viewer.usgs.aworldbridgelabs.com",
+
+    //upload path to geoserver when approved
+    geoServer : 'http://cs.aworldbridgelabs.com:8080/geoserver/',
+    // geoServer : 'http://10.11.90.15:8080/geoserver/',
+
+    //sysnchronization between approvedfolder and data folder under geoserver when approved
+    // Sync_Dir : '/usr/share/geoserver-2.15.0/data_dir/data/Approved',
+    // Sync_Dir : 'syncfolder',
+
+    //download/backup wmsCapabilities file (xml)
+    Download_From : 'https://cors.aworldbridgelabs.com:9084/http://cs.aworldbridgelabs.com:8080/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities',
+    Download_To:'../config/ows.xml',
+    Backup_Dir:'../config/geoCapacity',
+
+    //upload file--pending
+    Pending_Dir: 'uploadfolder',
+    Reject_Dir: 'rejectfolder',
+
+    //approve file--active
+    Approve_Dir: 'approvedfolder',
+
+    //trashfolder file--trashfolder
+    Delete_Dir: 'trashfolder',
+
+    num_backups: 24,
+    download_interval: 36000000,
+
+
+    mail: {
+        service: 'Gmail',
+        auth: {
+            // user: 'yge5095@gmail.com',
+            // pass: '1syyRFLATs%'
+            user: 'aaaa.zhao@g.northernacademy.org',
+            pass: 'qwer1234'
+        }
+    },
+    influx:{
+        precision: 'rfc3339',
+        host: 'aworldbridgelabs.com',
+        database: 'RayESP',
+        username: "rayf",
+        password: "RayESP8010",
+        port: 8086
+    },
+    //lab
+    mysql:{
+        host: "10.11.90.15",
+        // host: "localhost",
+        user: "AppUser",
+        password: "Special888%",
+        port: "3306",
+        Schema: "ESP2"
+        // Table: "stationdata"
+    },
+    //home
+    // mysql:{
+    //     // host: "10.11.90.15",
+    //     host: "localhost",
+    //     user: "root",
+    //     password: "RayESP8010",
+    //     port: "3306",
+    //     Schema: "ESP2"
+    //     // Table: "stationdata"
+    // },
+
+    PastTime:'3d',//This part controls
+    NowTime:'2m',//the time range shows in the index(home) page.
+
+    // ServerPort:"3005",
+    email:'esp_notify@northernacademy.org',
+    DegreeLimit:90,//if the degree is bigger than this limit, it will not send notification
+
+//UPDATE DATA INFO HERE
+    dateBeginning:'2021-03-31T16:17:18.795Z',
+    dateEnding:'2021-04-30T16:17:18.795Z',
+    duration:'1440m',//time duration of updateData, 1 days
+    durations:86400000,//miliseconds, 1days for rn
+
+
+    //in anomaly table, change the days ahead of event here
+    day:5,//day
+
+
+
+
+    //download/backup wmsCapabilities file (xml)
+    SpikeTimeLimit:1800000,//milliseconds, the longest time duration of a spike, 30 mins for rn
+    TimeAhead:"180m",//it decides how many time do we get for checking flags, m means minute
+    HourAhead:3,//unit: hour
+    // TimeAhead:"4320m",//it decides how many time do we get for checking flags, m means minute
+    // HourAhead:72,//unit: hour
+    Limit:8,//unit: nT
+    LimitStr:'8nT',//keep this as a string with a unit plz.
+    RepeatingTime:10800000,//unit:milli second, 3 hours for rn
+
+};
+
+module.exports = serverConfig;
---
+++ routes/routes.js
@@ -0,0 +1,76 @@
+// routes/routes.js
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const serverConfig = require('../config/serverConfig');
+const fs = require("fs");
+const fsextra = require('fs-extra');
+const request = require("request");
+const bcrypt = require('bcrypt-nodejs');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const async = require('async');
+const crypto = require('crypto');
+const rimraf = require("rimraf");
+const mkdirp = require("mkdirp");
+const multiparty = require('multiparty');
+const path    = require('path');
+// const ExpressBrute = require('express-brute');
+const rateLimit = require("express-rate-limit");
+const text = require('textbelt');
+const generator = require('generate-password');
+// const transporter = nodemailer.createTransport(serverConfig.mail);
+const Influx = require('influx');
+const influx = new Influx.InfluxDB(serverConfig.influx);
+const con = mysql.createConnection(serverConfig.mysql);
+var stations = [];
+var station;
+var DeEmail = serverConfig.email;
+var FlagN = [];
+var PairN = [];
+var EQstations;
+
+// const store = new ExpressBrute.MemoryStore(); // stores state locally, don't use this in production
+// const bruteforce = new ExpressBrute(store);
+
+const con_CS = mysql.createConnection(serverConfig.commondb_connection);
+const smtpTrans = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'aaaa.zhao@g.northernacademy.org',
+        pass: "qwer1234"
+    }
+});
+
+const Limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100,
+});
+
+let exec = require('child_process').exec;
+let myStat, myVal, myErrMsg, token, mylogin;
+let today, date2, date3, time2, time3, dateTime, tokenExpire;
+
+con_CS.query('USE ' + serverConfig.Login_db); // Locate Login DB
+
+module.exports = function (app, passport) {
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(cors({
+        origin: '*',
+        credentials: true
+    }));
+
+    // app.use(Limiter);
+
+    // =====================================
+    // CS APP Home Section =================
+    // =====================================
+
+    app.get('/',function (req,res) {
+        res.setHeader("Access-Control-Allow-Origin", "*"); // Allow cross domain header
+        res.render('homepage.ejs', {
+            message: req.flash('loginMessage'),
+            error: "Your username and password don't match."
+        })
+    });
