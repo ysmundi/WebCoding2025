e853db06a6dbfb0fca9cff1a63652eef4d216af2
anson-zhao <ansonzmj@gmail.com>
2025-02-27 22:46:35 -0500

    public server setup

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,11 +5,11 @@ dotenv.config();
 
 //Connecting to database
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || '10.11.90.15',
-    // host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'AppUser',
-    password: process.env.DB_PASSWORD || 'Special888%',
-    database: process.env.DB_NAME || 'fbla_webcoding',
+    // host: '10.11.90.15',
+    host: 'localhost',
+    user: 'AppUser',
+    password: 'Special888%',
+    database: 'fbla_webcoding',
 });
 
 db.connect((err) => {
