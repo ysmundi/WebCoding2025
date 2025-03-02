d85d8dfc4bd065f4dead0a293774faa1ac82e45d
imacbig08 <ngocbach.giap@g.northernacademy.org>
2025-03-02 16:20:21 -0500

    clear

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-const config = {
-     apiBaseUrl: 'http://10.11.20.195:3000' // Change this to your public URL when deploying
-    //apiBaseUrl: 'https://jobconn.northernhorizon.org' // Change this to your public URL when deploying
-    
-};
